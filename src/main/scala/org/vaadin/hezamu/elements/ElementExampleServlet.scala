7c2cfbcfe9c2a363bbe615477e98691e96df367b
Henri Muurimaa <henri@vaadin.com>
2015-01-23 15:56:44 -0800

    Inject script tag in Scaladin way

--- src/main/scala/org/vaadin/hezamu/elements/ElementExampleServlet.scala
+++ src/main/scala/org/vaadin/hezamu/elements/ElementExampleServlet.scala
@@ -7,27 +7,18 @@ import com.vaadin.server.BootstrapPageResponse
 import com.vaadin.server.BootstrapFragmentResponse
 import com.vaadin.server.SessionInitEvent
 import com.vaadin.server.SessionInitListener
-
-// Nasty hack until Scaladin supports @Javascript or some other way to inject script tags
-class BootstrapListenerImplementation extends BootstrapListener {
-  override def modifyBootstrapPage(response: BootstrapPageResponse) {
-    response.getDocument.head.appendElement("script").attr("src", "VAADIN/bower_components/webcomponentsjs/webcomponents.js")
-  }
-
-  override def modifyBootstrapFragment(response: BootstrapFragmentResponse) {
-    // nothing to do
-  }
-}
+import javax.servlet.ServletConfig
 
 @WebServlet(urlPatterns = Array("/*"), initParams = Array(new WebInitParam(name = "ScaladinUI", value = "org.vaadin.hezamu.elements.ElementExampleUI")))
 class ElementExampleServlet extends ScaladinServlet {
-  override def servletInitialized {
-    super.servletInitialized
+  // Maybe Scaladin should support a better way to inject script tags
+  override def init(servletConfig: ServletConfig) {
+    super.init(servletConfig)
 
-    getService.addSessionInitListener(new SessionInitListener() {
-      def sessionInit(event: SessionInitEvent) {
-        event.getSession.addBootstrapListener(new BootstrapListenerImplementation())
+    service.sessionInitListeners += { event =>
+      event.session.bootstrapPageListeners += { response =>
+        response.document.head.appendElement("script").attr("src", "VAADIN/bower_components/webcomponentsjs/webcomponents.js")
       }
-    })
+    }
   }
 }
